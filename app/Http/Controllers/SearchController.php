239b5656937ce580188bc370fc3b5fb48586a17f
Scotty Smith <chssmith@roanoke.edu>
2023-10-10 16:24:17 -0400

    Testing workflow?

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,18 +14,18 @@ class SearchController extends Controller {
         $request->validate(['search' => 'required']);
         $search_terms = explode(' ', $request->search);
 
-        $potential_users = User::where(function ($query) use ($search_terms) {
-            foreach ($search_terms as $term) {
-                $query->where(function ($search_query) use ($term) {
-                    $search_query->where('FirstName', 'LIKE', sprintf('%%%s%%', $term))
-                             ->orWhere('LastName', 'LIKE', sprintf('%%%s%%', $term))
-                             ->orWhere('MiddleName', 'LIKE', sprintf('%%%s%%', $term))
-                             ->orWhere('nick_name', 'LIKE', sprintf('%%%s%%', $term))
-                             ->orWhere('NickName', 'LIKE', sprintf('%%%s%%', $term))
-                             ->orWhere('RCID', 'LIKE', sprintf('%%%s%%', $term));
-                });
-            }
-        })->get()->map(function ($user) {
+$potential_users = User::where(function ($query) use ($search_terms) {
+foreach ($search_terms as $term) {
+$query->where(function ($search_query) use ($term) {
+$search_query->where('FirstName', 'LIKE', sprintf('%%%s%%', $term))
+->orWhere('LastName', 'LIKE', sprintf('%%%s%%', $term))
+->orWhere('MiddleName', 'LIKE', sprintf('%%%s%%', $term))
+->orWhere('nick_name', 'LIKE', sprintf('%%%s%%', $term))
+->orWhere('NickName', 'LIKE', sprintf('%%%s%%', $term))
+->orWhere('RCID', 'LIKE', sprintf('%%%s%%', $term));
+});
+}
+})->get()->map(function ($user) {
             $response_entry = collect();
             $response_entry['id'] = $user->RCID;
             $response_entry['display_data'] = view()->make('typeahead.typeahead', ['person' => $user])->render();
