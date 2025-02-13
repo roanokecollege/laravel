2d9992758126c3d9d5791f435f43b6bdca5bd57c
Scotty Smith <chssmith@roanoke.edu>
2025-02-13 14:36:06 -0500

    Adding newer laravel files

---
+++ bootstrap/providers.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    rcwebdevelopers\Auth\AuthServiceProvider::class,
+    rcwebdevelopers\mustangbuilder\MustangBuilderProvider::class,
+];
