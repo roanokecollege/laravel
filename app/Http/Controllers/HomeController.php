3d454b62d3b0b36ea415934f7f1b2eea5f495d31
chssmith <chssmith@users.noreply.github.com>
2026-09-21 16:33:39 +0000

    Fix PHPCS errors

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,27 +8,26 @@ use Illuminate\Http\Request;
 use Closure;
 use RCAuth;
 
-
 class HomeController extends Controller implements HasMiddleware
 {
 
     public static function middleware(): array {
         return [
-            function ($request, $next) {
-                $side_navigation = [
-                                    '<span class="far fa-fw fa-home"></span> Home' => action([self::class, 'index']),
-                                   ];
+                function ($request, $next) {
+                    $side_navigation = [
+                                        '<span class="far fa-fw fa-home"></span> Home' => action([self::class, 'index']),
+                                       ];
     
     
-                if (RCAuth::check() || RCAuth::attempt()){
-                    $side_navigation['<i class="far fa-fw fa-sign-out"></i> Logout ' . RCAuth::user()->username] = route('logout');
-                } else {
-                    $side_navigation['<i class="far fa-fw fa-sign-out"></i> Login'] = route('login');
-                }
-                view()->share('side_navigation', $side_navigation);
-                return $next($request);
-            },
-        ];
+                    if (RCAuth::check() || RCAuth::attempt()){
+                        $side_navigation['<i class="far fa-fw fa-sign-out"></i> Logout ' . RCAuth::user()->username] = route('logout');
+                    } else {
+                        $side_navigation['<i class="far fa-fw fa-sign-out"></i> Login'] = route('login');
+                    }
+                    view()->share('side_navigation', $side_navigation);
+                    return $next($request);
+                },
+               ];
     }
 
     public function index() {
