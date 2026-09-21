58a455938a037cfc6603a3f34c3a41f974dfa723
Scotty Smith <chssmith@roanoke.edu>
2026-09-21 11:25:08 -0400

    cbf fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
-    })->create();
+    ->withMiddleware(
+        function (Middleware $middleware): void {
+            //
+        }
+    )
+    ->withExceptions(
+        function (Exceptions $exceptions): void {
+            $exceptions->shouldRenderJsonWhen(
+                fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
+            );
+        }
+    )->create();
